7c101a1f1ce3ba0bc41afabbe3faf3bf8e90ebf4
gymirl <gymirlogi2@gmail.com>
2020-01-29 12:04:17 +0000

    work?

--- verkefni2/app.py
+++ verkefni2/app.py
@@ -3,11 +3,34 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
 @app.route('/')
-def home():
-    return render_template('default.html')
+def heima():
+    return render_template('index.html')
 
+
+#verk2.1
+@app.route('/verk2.1')
+def home1():
+    return render_template('kennitala.html')
+
+#listi (Database)
+#ID,Fyrirsögn,Frétt,Höfundur
+frettir=[
+    ['0','BROH','BIGGEST BRUH EVER!! MAN SAYS "BRUH" AND THE WHOLE CROWD CLAPS',"yesman.is"],
+    ['1','PLACEHOLDERNAME','PLACEHOLDERNAME','PLACEHOLDERNAME.is'],
+    ['2','PASS','PASS','PASS.is'],
+    ['3','PASS','PASS','PASS.is']
+]
+
+#2.2hluti
+@app.route('/verk2.2')
+def seinnihluti():
+    return render_template('frettir.html',frettir=frettir)
+
+#frett
+@app.route('/frett/<int:id>')
+def news(id):
+    return render_template('frett.html', frett=frettir[id], nr=id)
 
 @app.route('/ktsida/<kt>')
 def ktsida(kt):
